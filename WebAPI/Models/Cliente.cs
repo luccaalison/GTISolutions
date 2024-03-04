a1ff93dc2322b389f581c0e761d905946c082bc0
luccaalison <38993023+luccaalison@users.noreply.github.com>
2024-03-03 22:57:38 -0300

    salvando alterações

--- WebAPI/Models/Cliente.cs
+++ WebAPI/Models/Cliente.cs
@@ -13,10 +13,10 @@ namespace WebAPI.Models
         public string CPF { get; set; }
         public string Nome { get; set; }
         public string RG { get; set; }
-        public DateTime DataExpedicao { get; set; }
+        public string DataExpedicao { get; set; }
         public string OrgaoExpedicao { get; set; }
-        public string UF { get; set; }
-        public DateTime DataNascimento { get; set; }
+        public string UFExpedicao { get; set; }
+        public string DataNascimento { get; set; }
         public string Sexo { get; set; }
         public string EstadoCivil { get; set; }
         public EnderecoCliente Endereco { get; set; }
